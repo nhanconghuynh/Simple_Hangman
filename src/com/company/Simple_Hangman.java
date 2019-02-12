dc088bbc67ed27d89179a5c39bbc328513e20600
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-02-12 16:00:51 -0500

    Second Commit _ Fully Working Version

--- src/com/company/Simple_Hangman.java
+++ src/com/company/Simple_Hangman.java
@@ -20,11 +20,7 @@ The user can input either a letter or a word for their guess.
 If the user guesses the word correctly, output "You've won!" and end the game.
 */
 
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
-import java.util.HashMap;
+import java.util.*;
 
 public class Simple_Hangman {
 
@@ -34,12 +30,6 @@ public class Simple_Hangman {
         ArrayList<Character> word = new ArrayList<Character>();
 
         Random rand_word = new Random();
-
-
-
-
-
-
 
         String[] dictionary = {"tree", "rain" , "bear", "encourage", "promise", "soup", "chess", "insurance", "pancakes", "stream"};
 
@@ -68,19 +58,28 @@ public class Simple_Hangman {
 
     private static boolean check_win(ArrayList<Character> mystery_word, ArrayList<Character> guess_word) {
         int count = 0;
-        for (int i = 0; i < mystery_word.size(); i++) {
-            if (guess_word.get(i) == mystery_word.get(i))
-                count++;
+
+        if (guess_word.size() != mystery_word.size())
+            return false;
+
+        else {
+
+            for (int i = 0; i < mystery_word.size(); i++) {
+                if (guess_word.get(i) == mystery_word.get(i))
+                    count++;
+            }
+
+            return (count == mystery_word.size());
         }
 
-        return (count == mystery_word.size());
-
         }
+
+
 
     public static void main(String[] args) {
         ArrayList<Character> random_word = new ArrayList<Character>();
         ArrayList<Character> guess_word = new ArrayList<Character>();
-
+        ArrayList<Character> guess_check = new ArrayList<Character>();
 
 
         System.out.println("Welcome, let's play hangman!");
@@ -97,41 +96,80 @@ public class Simple_Hangman {
         String another = "n";
         String response = "";
         int wrong_count = 0;
+        int letter_count = 0;
         boolean check = false;
+        boolean another_option = true;
+        boolean loop = true;
+
 
         char[] stringToCharArray = new char[1];
 
         do {
-            System.out.print("\n\nEnter your guess: ");
+
+            System.out.print("\nEnter your guess: ");
             String guess = scanner.next();
-            char guess_char = guess.toCharArray()[0];
+
+            guess_check = new ArrayList<Character>();
+            for (char c: guess.toCharArray()) {
+                guess_check.add(c);
+            }
+
+            char guess_char = guess_check.get(0);
 
             check = check_win(random_word, guess_word);
 
+
+            if (check_win(random_word, guess_check)) {
+                System.out.println("You've won. The word was ");
+                for (char c:random_word) {
+                    System.out.print(c);
+                }
+                System.out.print(".");
+                check = true;
+                another_option = false;
+                loop = false;
+            }
+
                 if ((check_letter(random_word, guess_char)) && (wrong_count <5) && (!check)) {
-                    System.out.println("Good job. There is/are a/an '" + guess_char + "' in the mystery word.");
+
+                        letter_count = 0;
                         for (int i=0; i < random_word.size(); i++) {
-                            if (guess_char == random_word.get(i))
-                            guess_word.set(i, guess_char);
+                            if (guess_char == random_word.get(i)) {
+                                guess_word.set(i, guess_char);
+                                letter_count++;
+                            }
                         }
+
+                    if (letter_count == 1)
+                        System.out.println("Good job. There is "+ letter_count + " '" + guess_char + "' in the mystery word.");
+
+                    if (letter_count > 1)
+                        System.out.println("Good job. There are "+ letter_count + " '" + guess_char + "'s in the mystery word.");
+
+                    if (!check_win(random_word, guess_word)) {
                         formatted = guess_word.toString().replace("[", "").replace("]", "").replace(",", "");
                         System.out.println("What you've guessed so far: " + formatted);
-
-                    System.out.println("\nDo you want to continue guessing? (y/n)");
-                    another = scanner.nextLine();
-
+                        System.out.println("You have guessed incorrectly " + wrong_count + "/6 times.");
+                        System.out.println("\nDo you want to continue guessing? (y/n)");
+                        another = scanner.next();
+                        another_option = true;
+                        loop = true;
                     }
 
-                else if ( (wrong_count < 5) && !(check_win(random_word, guess_word)) ) {
+                }
+
+                else if ( (wrong_count < 5) && (!check) ) {
                     System.out.println("Sorry. There is no '" + guess_char + "' in the mystery word.");
                     wrong_count += 1;
                     System.out.println("You have guessed incorrectly " + wrong_count + "/6 times.");
                     formatted = guess_word.toString().replace("[", "").replace("]", "").replace(",", "");
                     System.out.println("What you've guessed so far: " + formatted);
                     System.out.println("\nDo you want to continue guessing? (y/n)");
-                    another = scanner.nextLine();
+                    another = scanner.next();
+                    another_option = true;
+                    loop = true;
                 }
-                else if ( (wrong_count == 5) && !(check_win(random_word, guess_word)) ) {
+                else if ( (wrong_count == 5) && (!check) ) {
                     System.out.println("Sorry. There is no '" + guess_char + "' in the mystery word.");
                     wrong_count += 1;
                     System.out.println("You have guessed incorrectly " + wrong_count + "/6 times.");
@@ -140,6 +178,8 @@ public class Simple_Hangman {
                         System.out.print(value);
                     }
                     System.out.print(".");
+                    loop = false;
+                    another_option = false;
                 }
 
                 if (check_win(random_word, guess_word)) {
@@ -148,22 +188,50 @@ public class Simple_Hangman {
                         System.out.print(value);
                     }
                     System.out.print(".");
+                    another_option = false;
+                    loop = false;
                 }
 
-        } while ((another.equals("y")) || (another.equals("Y")) || (another.equals("yes")) || (another.equals("YES")) || (another.equals("Yes")) ||
-                (wrong_count <= 5) || (check));
+
+
+            while (loop) {
+                if
+                (
+                        (another.equalsIgnoreCase("y")) ||
+                                (another.equalsIgnoreCase("yes"))
+                ) {
+                    another_option = true;
+                    loop = false;
+                }
+                else if
+                (
+                        (another.equalsIgnoreCase("n")) ||
+                                (another.equalsIgnoreCase("no"))
+                ) {
+
+                    loop = false;
+
+                }
+
+                else
+                {
+                    System.out.println("Invalid input. Would you like to continue guessing? (y/n or yes/no)   ");
+                    another = scanner.next();
+                }
+            }
+
+            if
+            (
+                    (another.equalsIgnoreCase("n")) ||
+                            (another.equalsIgnoreCase("no"))
+            ) {
+                System.out.println("Too bad you don't want to play anymore. Exiting...");
+                another_option = false;
+            }
 
 
 
-/*        System.out.print("\n\nThe word was ");
-        for (char value:random_word) {
-            System.out.print(value);
-        }
-        System.out.print(".");
-*/
-
-
-
+        } while (another_option);
 
     }
 }
